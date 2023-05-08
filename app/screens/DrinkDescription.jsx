c06ee279d07186525e6c05c945955b317ba5028d
JessieSu2 <JessieSu3@gmail.com>
2023-05-08 02:18:16 -0400

    can now display drink information

--- app/screens/DrinkDescription.jsx
+++ app/screens/DrinkDescription.jsx
@@ -1,5 +1,5 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
 import { Icon } from "react-native-elements";
 import { auth, db } from "../../firebase";
@@ -10,72 +10,138 @@ import {
   arrayUnion,
   arrayRemove,
   deleteField,
+  onSnapshot,
 } from "firebase/firestore";
+import { useIsFocused } from "@react-navigation/native";
+import { Alert } from "react-native";
 const removeKeyIfEmpty = async (keyToRemove, favoriteStoreKeyPath, uid) => {
-  try {
-    const docRef = doc(db, "users", uid);
-    const documentSnapshot = await getDoc(docRef);
-    if (documentSnapshot.exists()) {
-      const data = documentSnapshot.data();
-      if (data && data.favorites && data.favorites[keyToRemove].length === 0) {
-        await updateDoc(docRef, {
-          [`${favoriteStoreKeyPath}`]: deleteField(),
-        });
-        console.log("Array removed successfully");
+  if (uid) {
+    try {
+      const docRef = doc(db, "users", uid);
+      const documentSnapshot = await getDoc(docRef);
+      if (documentSnapshot.exists()) {
+        const data = documentSnapshot.data();
+        if (
+          data &&
+          data.favorites &&
+          data.favorites[keyToRemove].length === 0
+        ) {
+          await updateDoc(docRef, {
+            [`${favoriteStoreKeyPath}`]: deleteField(),
+          });
+          console.log("Array removed successfully");
+        }
       }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
+  } else {
+    console.log("removeKeyIfEmpty::No account");
   }
 };
 
 const updateAddUserFavorites = async (drinkId, storeKey) => {
-  const currentUser = auth.currentUser;
-  const uid = currentUser.uid;
-  const favoriteStoreKeyPath = `favorites.${storeKey}`;
-  try {
-    const docRef = doc(db, "users", uid);
-    const documentSnapshot = await getDoc(docRef);
-    const favorites = documentSnapshot.data().favorites;
-    updateDoc(
-      doc(db, "users", `${uid}`),
-      {
-        [`${favoriteStoreKeyPath}`]: arrayUnion(drinkId),
-      },
-      { merge: true }
-    ).catch((error) => {
-      console.log("couldnt update doc");
-      alert(error.message);
-    });
-  } catch (error) {
-    console.log(error);
+  const uid = auth.currentUser?.uid;
+  if (uid) {
+    const favoriteStoreKeyPath = `favorites.${storeKey}`;
+    try {
+      const docRef = doc(db, "users", uid);
+      const documentSnapshot = await getDoc(docRef);
+      const favorites = documentSnapshot.data().favorites;
+      updateDoc(
+        doc(db, "users", `${uid}`),
+        {
+          [`${favoriteStoreKeyPath}`]: arrayUnion(drinkId),
+        },
+        { merge: true }
+      ).catch((error) => {
+        console.log("couldnt update doc");
+        alert(error.message);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("updateAddUserFav:: No Account");
   }
 };
+
 const updateRemoveUserFavorites = async (drinkId, storeKey) => {
-  const currentUser = auth.currentUser;
-  const uid = currentUser.uid;
-  const favoriteStoreKeyPath = `favorites.${storeKey}`;
-  try {
-    updateDoc(
-      doc(db, "users", `${uid}`),
-      {
-        [`${favoriteStoreKeyPath}`]: arrayRemove(drinkId),
-      },
-      { merge: true }
-    ).catch((error) => {
-      console.log("couldnt update doc");
-      alert(error.message);
-    });
-  } catch (error) {
-    console.log(error);
+  const uid = auth.currentUser?.uid;
+  if (uid) {
+    const favoriteStoreKeyPath = `favorites.${storeKey}`;
+    try {
+      updateDoc(
+        doc(db, "users", `${uid}`),
+        {
+          [`${favoriteStoreKeyPath}`]: arrayRemove(drinkId),
+        },
+        { merge: true }
+      ).catch((error) => {
+        console.log("couldnt update doc");
+        alert(error.message);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("updateRemoveUserFav::No account");
+  }
+};
+
+const updateLiked = async (drinkId, liked) => {
+  const uid = auth.currentUser?.uid;
+  if (uid) {
+    try {
+      const docRef = doc(db, "favorites", uid);
+      const documentSnapshot = await getDoc(docRef);
+      updateDoc(
+        doc(db, "favorites", `${uid}`),
+        {
+          [drinkId]: liked,
+        },
+        { merge: true }
+      ).catch((error) => {
+        console.log("couldnt update doc");
+        alert(error.message);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("UpdateLiked::No account");
+  }
+};
+
+const GetLikedData = async (selectedDrinkData) => {
+  const currentUser = auth.currentUser?.uid;
+  if (currentUser) {
+    const docRef = doc(db, "favorites", `${currentUser}`);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      data = docSnap.data();
+      console.log("GetLikedData", data[selectedDrinkData.drinkid]);
+      if (data[selectedDrinkData.drinkid] == true) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+      return false;
+    }
+  } else {
+    console.log("not logged in");
+    return false;
   }
 };
 
 const DrinkDescription = ({ route }) => {
-  const currentUser = auth.currentUser;
-  // console.log("You are ", currentUser);
-  const uid = currentUser.uid;
+  const uid = auth.currentUser?.uid;
   const [liked, setLiked] = useState(false);
+  const [howTo, setHowTo] = useState("");
+  const [showTo, setShowTo] = useState();
   const selectedDrinkData = route.params.selectedDrink;
   const favoriteStoreKeyPath = `favorites.${selectedDrinkData.storekey}`;
   // console.log("DrinkDescription::route ", route);
@@ -83,27 +149,74 @@ const DrinkDescription = ({ route }) => {
   const selectedDrinkName = selectedDrinkData.name;
   // console.log("DrinkDescriptionData::storkey", selectedDrinkData.storekey);
   // console.log("DrinkDescriptionData::drinkid", selectedDrinkData.drinkid);
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (isFocused) {
+          const isLiked = await GetLikedData(selectedDrinkData);
+          setLiked(isLiked);
+          console.log("UseEffect", isLiked);
+        }
+      } catch (error) {
+        console.error(error);
+        setLiked(false);
+      }
+    };
+
+    fetchData();
+
+    // Clean-up function here (if needed)
+  }, [isFocused]);
+
   const toggleLike = () => {
     // Update the liked state67
-    setLiked(!liked);
-    console.log(!liked);
-    if (!liked) {
-      console.log("Added Favorites");
-      updateAddUserFavorites(
-        selectedDrinkData.drinkid,
-        selectedDrinkData.storekey
-      );
+    if (uid) {
+      setLiked(!liked);
+      console.log(!liked);
+      if (!liked) {
+        console.log("Added Favorites");
+        updateAddUserFavorites(
+          selectedDrinkData.drinkid,
+          selectedDrinkData.storekey
+        );
+        updateLiked(selectedDrinkData.drinkid, !liked);
+      } else {
+        console.log("Removed Favorites");
+        updateRemoveUserFavorites(
+          selectedDrinkData.drinkid,
+          selectedDrinkData.storekey
+        );
+        updateLiked(selectedDrinkData.drinkid, !liked);
+        removeKeyIfEmpty(selectedDrinkData.storekey, favoriteStoreKeyPath, uid);
+      }
     } else {
-      console.log("Removed Favorites");
-      updateRemoveUserFavorites(
-        selectedDrinkData.drinkid,
-        selectedDrinkData.storekey
+      Alert.alert("Cant like if not logged");
+    }
+  };
+  const fetchData = async () => {
+    try {
+      const docRef = doc(
+        db,
+        `stores/${selectedDrinkData.storekey}/drinks`,
+        `${selectedDrinkData.drinkid}`
       );
-      removeKeyIfEmpty(selectedDrinkData.storekey, favoriteStoreKeyPath, uid);
+      const querySnapshot = await getDoc(docRef);
+
+      const howToOrder = querySnapshot.get("howTo");
+      setHowTo(howToOrder);
+      // console.log(showTo);
+      const showBaristaData = querySnapshot.get("showBarista");
+      setShowTo(showBaristaData);
+    } catch (error) {
+      console.log(error);
     }
   };
 
   const TopSection = () => {
+    useEffect(() => {
+      fetchData();
+    });
     return (
       <>
         <View style={styles.imageContainer}>
@@ -138,11 +251,7 @@ const DrinkDescription = ({ route }) => {
         <View style={styles.middle}>
           {/* Title */}
           <Text style={styles.title}>How To Order</Text>
-          <Text>
-            “Can I get a grande Mango Dragonfruit Lemonade with no dragonfruit
-            inclusions, and a splash of peach juice, then topped with Iced
-            Passion Tango Tea so that it's layered?”
-          </Text>
+          <Text>{howTo}</Text>
         </View>
 
         <Text style={styles.or}>or</Text>
@@ -160,13 +269,8 @@ const DrinkDescription = ({ route }) => {
         <FlatList
           style={styles.bottom}
           showsVerticalScrollIndicator={false}
-          data={[
-            { key: "[Size] Mango Dragonfruit Lemonade" },
-            { key: "No Mango Dragonfruit Inclusions" },
-            { key: "Add peach juice" },
-            { key: "Add passion tango on top" },
-          ]}
-          renderItem={({ item }) => <Text>{`\u2043 ${item.key} `}</Text>}
+          data={showTo}
+          renderItem={({ item }) => <Text>{`\u2043 ${item} `}</Text>}
           ListHeaderComponent={TopSection}
         />
       </View>
@@ -177,12 +281,12 @@ const styles = StyleSheet.create({
   // Whole container
   container: {
     alignItems: "center",
-    backgroundColor: "#ffc2c2",
+    // backgroundColor: "#ffc2c2",
     flex: 1,
   },
   // Inner container
   sizeOfColumn: {
-    paddingHorizontal: 15,
+    // paddingHorizontal: 15,
   },
   // Image
   imageContainer: {
